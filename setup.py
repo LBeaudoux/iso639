d377f5fd0c4cc1ed2d8421164ff7ce49e61dd0ce
lbdx <lbdx@x55vd.home>
2020-05-18 17:35:22 +0200

    version 0.0.3

--- setup.py
+++ setup.py
@@ -4,20 +4,20 @@ with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="iso639-l-bdx",
-    version="0.0.2",
+    name="iso639-lang",
+    version="0.0.3",
     author="L.Beaudoux",
-    author_email="lbeaudoux@gmail.com",
-    description="A Python 3.6 library for ISO 639 language codes.",
+    description="A simple library for ISO 639 language codes.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/l-bdx/iso639",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
     ],
     python_requires=">=3.6",
-    package_data={"": ["*.tab", "*.json"]},
+    package_data={"": ["*.tab"]},
 )
