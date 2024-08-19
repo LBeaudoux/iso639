171e8364b3c89c156429fde39682a339f392c806
lbdx <lbeaudoux@gmail.com>
2024-08-19 09:50:36 +0200

    Move language iterator to the main module

--- iso639/iso639.py
+++ iso639/iso639.py
@@ -1,7 +1,7 @@
 from operator import itemgetter
-from typing import Dict, List, Union
+from typing import Dict, Iterator, List, Union
 
-from .datafile import load_mapping
+from .datafile import load_langs, load_mapping
 from .exceptions import DeprecatedLanguageValue, InvalidLanguageValue
 
 
@@ -301,3 +301,16 @@ class Lang(tuple):
         cls._type = load_mapping("mapping_type")
         cls._deprecated = load_mapping("mapping_deprecated")
         cls._macro = load_mapping("mapping_macro")
+
+
+def iter_langs() -> Iterator[Lang]:
+    """Iterate through all not deprecated ISO 639 languages
+
+    Yields
+    -------
+    Lang
+        Lang instances ordered alphabetically by name
+    """
+    sorted_langs = load_langs()
+
+    return iter(sorted_langs)
